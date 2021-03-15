38eed13dd5656326af446635ee13208e7c585ef6
Toshinobu-okada <okacchi_nobu@yahoo.co.jp>
2021-03-15 13:18:46 +0000

    [Update]画像投稿機能、いいねコメント機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6d1d78ca15510c84e2015147fc2e3dc199ed6623c0221b664dd11437c1887187e65bd1de198fd73a29ade79bd468c25326477a2e4107ff646003c69abdd39703'
